a96da5317954344861eecbc1430d3471d782e42f
MJMMiller <mateomiller1605@gmail.com>
2023-12-01 08:22:54 -0600

    Uso de loadChildren ✅
    Define las rutas  en el archivo de routing de cada tab

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -7,29 +7,30 @@ const routes: Routes = [
     path: 'tabs',
     component: TabsPage,
     children: [
-      {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+      
+      {//Instanciar pa pagina hija de inicio
+        path: 'inicio',
+        loadChildren: () => import('./inicio/inicio.module').then( hijo => hijo.InicioPageModule)
       },
-      {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+      {//Instanciar pa pagina hija de inicio
+        path: 'nudo',
+        loadChildren: () => import('./nudo/nudo.module').then( hijo => hijo.NudoPageModule)
       },
-      {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      {//Instanciar pa pagina hija de inicio
+        path: 'desenlace',
+        loadChildren: () => import('./desenlace/desenlace.module').then( hijo => hijo.DesenlacePageModule)
       },
-      {
+      {//Hija por defecto
         path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
-      }
+        redirectTo:"inicio",
+        pathMatch:"full"
+      },
     ]
   },
-  {
+  {//Padre por defecto
     path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
+    redirectTo:"tabs",
+    pathMatch:"full"
   }
 ];
 
